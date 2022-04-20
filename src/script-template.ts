431e29024a7c574f242cdf543619a2d7e0a86e25
cameron-boyt <cameronboyt@gmail.com>
2022-04-20 23:57:48 +0100

    Script template updates for future overhaul

--- src/script-template.ts
+++ src/script-template.ts
@@ -1,22 +1,21 @@
-import { NS } from '@ns'
-import { genPlayer, IPlayerObject } from '/libraries/player-factory';
-import { genServer, IServerObject } from '/libraries/server-factory';
-import { ScriptLogger } from '/libraries/script-logger';
+import { NS } from "@ns";
+import { PortNumber, purgePort, writeToPort } from '/libraries/port-handler';
+import { MessageType, ScriptLogger } from "/libraries/script-logger";
 
 // Script logger
-let logger : ScriptLogger;
+let logger: ScriptLogger;
 
 // Script refresh period
 const refreshPeriod = 1000;
 
 // Flags
-const flagSchema : [string, string | number | boolean | string[]][] = [
-	["h", false],
-	["help", false],
+const flagSchema: [string, string | number | boolean | string[]][] = [
+    ["h", false],
+    ["help", false],
     ["v", false],
     ["verbose", false],
     ["d", false],
-    ["debug", false]
+    ["debug", false],
 ];
 
 // Flag set variables
@@ -26,79 +25,158 @@ let debug = false; // Log in debug mode
 
 /*
  * > SCRIPT VARIABLES <
-*/
+ */
 
-/** Player object */
-let player : IPlayerObject;
-/** This machine object */
-let machine : IServerObject;
+/** Example variable. **/
+let argCatcher = 1;
 
 /*
  * ------------------------
- * > ENVIRONMENT SETUP FUNCTION
+ * > SCRIPT RUN TEST FUNCTIONS
  * ------------------------
-*/
+ */
+
+/**
+ * Test if this script can/should be run.
+ * @param _ns NS object.
+ * @returns True if the script can be run; false otherwise.
+ */
+async function canRunScript(_ns: NS): Promise<boolean> {
+    return true;
+}
+
+/*
+ * ------------------------
+ * > ENVIRONMENT SETUP FUNCTIONS
+ * ------------------------
+ */
 
 /**
  * Set up the environment for this script.
- * @param ns NS object parameter.
+ * @param _ns NS object.
  */
-function setupEnvironment(ns : NS) : void {
-    player = genPlayer(ns);
-    machine = genServer(ns, ns.getHostname());
+async function setupEnvironment(_ns: NS): Promise<void> {
+    logger.log(argCatcher.toString());
+}
 
-	player.money;
-	machine.hostname;
+/*
+ * ------------------------
+ * > DATA UPDATE FUNCTIONS
+ * ------------------------
+ */
+
+/**
+ * Test if this script can/should be run.
+ * @param ns NS object.
+ * @returns True if the script can be run; false otherwise.
+ */
+async function updateData(ns: NS): Promise<void> {
+    logger.log("Updating data...", { type: MessageType.debugLow });
+    ns.prompt("Do update?");
+}
+
+/*
+ * ------------------------
+ * > DO SCRIPT PROCESSING FUNCTIONS
+ * ------------------------
+ */
+
+/**
+ * Execute the funtionality of this script.
+ * @param ns NS object.
+ */
+async function doScriptFunctions(ns: NS): Promise<void> {
+    logger.log("Performing script functions...", { type: MessageType.debugLow });
+    await ns.asleep(refreshPeriod);
+}
+
+/*
+ * ------------------------
+ * > DATA EXPORT FUNCTIONS
+ * ------------------------
+ */
+
+/**
+ * Package and export script data to a port to be used by other scripts.
+ * @param ns NS object.
+ */
+async function exportData(ns: NS): Promise<void> {
+    logger.log("Exporting data...", { type: MessageType.debugLow });
+    purgePort(ns, PortNumber.CodingContractSolution)
+    await writeToPort(ns, PortNumber.CodingContractSolution, "");
+}
+
+/*
+ * ------------------------
+ * > SCRIPT CYCLE WAIT FUNCTIONS
+ * ------------------------
+ */
+
+/**
+ * Wait an amount of time before the script starts its next cycle.
+ * @param ns NS object.
+ */
+async function waitForScriptCycle(ns: NS): Promise<void> {
+    logger.log("Waiting for script sleep cycle...", { type: MessageType.debugLow });
+    await ns.asleep(refreshPeriod);
 }
 
 /*
  * ------------------------
  * > MAIN LOOP
  * ------------------------
-*/
+ */
 
-/** @param ns NS object parameter */
-export async function main(ns : NS) : Promise<void> {
-	ns.disableLog("ALL");
+/** @param ns NS object */
+export async function main(ns: NS): Promise<void> {
+    ns.disableLog("ALL");
     logger = new ScriptLogger(ns, "TEST", "TEST");
 
-	// Parse args
-	ns.args[0];
+    // Parse args
+    argCatcher = ns.args[0] as number;
 
-	// Parse flags
-	const flags = ns.flags(flagSchema);
-	help = flags.h || flags["help"];
-	verbose = flags.v || flags["verbose"];
-	debug = flags.d || flags["debug"];
+    // Parse flags
+    const flags = ns.flags(flagSchema);
+    help = flags.h || flags["help"];
+    verbose = flags.v || flags["verbose"];
+    debug = flags.d || flags["debug"];
 
-	if (verbose) logger.setLogLevel(2);
-	if (debug) 	 logger.setLogLevel(3);
+    if (verbose) logger.setLogLevel(2);
+    if (debug) logger.setLogLevel(3);
 
-	// Helper output
-	if (help) {
-		ns.tprintf('%s',
-			`TEST Helper\n`+
-			`Description:\n` +
-			`   Test script.\n` +
-			`Usage:\n` +
-			`   run /.js [args] [flags]\n` +
-			`Arguments:\n` +
-			`   exampleArg : string |>> This is an argument.\n` +
-			`Flags:\n` +
-			`   -h or --help    : boolean |>> Prints this.\n` +
-			`   -v or --verbose : boolean |>> Sets logging level to 2 - more verbosing logging.\n` +
-			`   -d or --debug   : boolean |>> Sets logging level to 3 - even more verbosing logging.`
-		);
+    // Helper output
+    if (help) {
+        ns.tprintf(
+            "%s",
+            `TEST Helper\n` +
+                `Description:\n` +
+                `   Test script.\n` +
+                `Usage:\n` +
+                `   run /.js [args] [flags]\n` +
+                `Arguments:\n` +
+                `   exampleArg : string |>> This is an argument.\n` +
+                `Flags:\n` +
+                `   -h or --help    : boolean |>> Prints this.\n` +
+                `   -v or --verbose : boolean |>> Sets logging level to 2 - more verbosing logging.\n` +
+                `   -d or --debug   : boolean |>> Sets logging level to 3 - even more verbosing logging.`,
+        );
 
-		return;
-	}
+        return;
+    }
 
-	setupEnvironment(ns);
+    if (!(await canRunScript(ns))) {
+        logger.log("Conditions to run script are not met; exiting.", { type: MessageType.warning });
+		ns.exit();
+    }
 
-	logger.initialisedMessage(true, false);
+    await setupEnvironment(ns);
 
-	while (true) {
-		await ns.asleep(refreshPeriod);
-		break;
-	}
+    logger.initialisedMessage(true, false);
+
+    while (true) {
+        await updateData(ns);
+        await doScriptFunctions(ns);
+        await exportData(ns);
+        await waitForScriptCycle(ns);
+    }
 }
