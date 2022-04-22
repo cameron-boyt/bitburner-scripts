81828829cc0b33d976a21bbb7f7f6161f55496d1
cameron-boyt <cameronboyt@gmail.com>
2022-04-22 21:59:07 +0100

    Change uid type to string

--- src/bladeburner/dodger/getActionCountRemaining-bulk.ts
+++ src/bladeburner/dodger/getActionCountRemaining-bulk.ts
@@ -2,7 +2,7 @@ import { NS } from "@ns";
 import { IBladeburnerAction } from "/bladeburner/bladeburner-data";
 
 export async function main(ns: NS): Promise<void> {
-    const uid = ns.args[0] as number;
+    const uid = ns.args[0] as string;
     const actions: IBladeburnerAction[] = JSON.parse(ns.args[1] as string);
 
     const result = actions.map((action) => ns.bladeburner.getActionCountRemaining(action.type, action.name));
